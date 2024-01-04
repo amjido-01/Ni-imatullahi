c8ec12f124fd7380636ac013c5d2ed8a5a5c2659
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-04 07:54:20 +0100

    working on the hero section

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -7,8 +7,14 @@ export const Hero: React.FunctionComponent = () => {
     <div className='h-screen border-2 hero'>
       <HamburgerMenu />
 
-      <div className='border-2 mt-[18.6rem]'>
-          <h1>Crafting Engaging UI/UX Experiences</h1>
+      <div className='border-2 mt-[18.69rem] md:mt-[18.6rem] flex flex-col items-center justify-center'>
+        <div>
+           <div className="heading">
+              <h1 className='w-[18.5625rem] h-[12.0625rem] border-2 md:w-[68.3125rem] md:h-[14.625rem]'>Crafting Engaging UI/UX Experiences</h1>
+           </div>
+           <div className="sub-heading"></div>
+        </div>
+        <div className="call"></div>
       </div>
     </div>
   )
