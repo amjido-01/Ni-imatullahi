b5e441fca0f76554c92c329867d900df75bdd8f7
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-05 16:40:05 +0100

    working on the hero section

---
+++ src/components/ui/Gallery.tsx
@@ -0,0 +1,9 @@
+export const Gallery: React.FunctionComponent = () => {
+    return (
+        <div className="h-[400px] border-2 mt-8 flex justify-center items-center">
+            <h1 className="text-4xl text-white">
+                GALLERY
+            </h1>
+        </div>
+    )
+}
