f68b6c2c832f3dcf4230eed5e9c446e592e34719
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-05 12:27:27 +0100

    hero section adjustment

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -10,24 +10,23 @@ export const Hero: React.FunctionComponent = () => {
       
       <div className='flex flex-col justify-center items-centr'>
 
-        <div className='w-[90%] md:w-[80%] mx-auto mt-[12.69rem] md:mt-[18.6rem]'>
+        <div className='w-[86%] md:w-[80%] mx-auto mt-[16.69rem] md:mt-[13rem]'>
            <div className="heading">
-              <h1 className='text-[#F4F4F4] leading-[120%] tracking[2px] uppercase font-black style w-[18.5625rem] h[12.0625rem] border-2 md:w-[100%] md:h[14.625rem] text-[3rem] md:text-[6rem]'>Crafting Engaging UI/UX Experiences</h1>
+              <h1 className='text-[#F4F4F4] leading-[120%] tracking[2px] uppercase font-black style w-[18.5625rem] h[12.0625rem] md:w-[100%] md:h[14.625rem] heads md:text-[6rem]'>Crafting Engaging UI/UX Experiences</h1>
            </div>
            
-           <div className="flex items-center justify-start mt-[2rem] md:mt-[1.5rem] md:justify-end sub-heading">
+           <div className="flex items-center justify-start mt-[2rem] md:mt-[1rem] md:justify-end sub-heading">
             
-            <p className='text-[#aaa] w-[15.8125rem] h-[3.625rem] md:w-[29.625rem] md:h-[5.4375rem] border2 text-[0.875rem] md:text-[1.5rem] style font-normal leading-[120%]'>Deliver seamless design and  intuitive functionality for web and mobile apps that enhance user engagement</p>
+            <p className='text-[#aaa] w-[15.8125rem] h-[3.625rem] md:w-[29.625rem] md:h-[5.4375rem] text-[0.875rem] md:text-[1.5rem] style font-normal leading-[120%]'>Deliver seamless design and  intuitive functionality for web and mobile apps that enhance user engagement</p>
             
            </div>
 
-             <div className="flex border-green-500 mt-[3rem] mx-auto md:mt-[4.5rem] call">
-
-          <Button variant='link' className='text-[#F4F4F4] text-[1rem] flex gap-[0.75rem] items-center md:text-[1.5rem] style font-bold leading-[120%] px-0'>
-              Visit my works
-            <VisitArrow className='md:mt-1 md:w-[1.375rem] md:h-[1.375rem]'/>
-          </Button>
-        </div>
+            <div className="flex border-green-500 mt-[3rem] mx-auto md:mt-[3rem] call">
+              <Button variant='link' className='text-[#F4F4F4] text-[1rem] flex gap-[0.75rem] items-center md:text-[1.5rem] style font-bold leading-[120%] px-0'>
+                  Visit my works
+              <VisitArrow className='md:mt-1 md:w-[1.375rem] md:h-[1.375rem]'/>
+            </Button>
+          </div>
         </div>
         
       
