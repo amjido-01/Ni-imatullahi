e71ea5242fa2d6621a82b2518fe9f1d2b8e18efe
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-06 20:51:32 +0100

    working on the hero section

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -1,9 +1,26 @@
+import work from '@/assets/work.png'
 export const Gallery: React.FunctionComponent = () => {
     return (
-        <div className="h-[400px] border-2 mt-8 flex justify-center items-center">
-            <h1 className="text-4xl text-white">
-                GALLERY
-            </h1>
+        <div className="border-2 mt-[7.12rem] flex flex-col gap-[2.54rem] md:gap-[1.5rem]">
+           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[2.54rem] md:gap-[1.5rem]">
+            <div className='w-[90%] md:w-[45%] md:h-[35.2rem]'>
+                <img src={work} alt="work" />
+            </div>
+             <div className='w-[90%] md:w-[45%] md:h-[35.2rem]'>
+                <img src={work} alt="work" />
+            </div>
+           </div>
+
+           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[2.54rem] md:gap-[1.5rem]">
+            <div className='w-[90%] md:w-[45%] md:h-[35.2rem]'>
+                <img src={work} alt="work" />
+            </div>
+             <div className='w-[90%] md:w-[45%] md:h-[35.2rem]'>
+                <img src={work} alt="work" />
+            </div>
+           </div>
+
+          
         </div>
     )
 }
--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -5,12 +5,12 @@ import { HamburgerMenu } from './HamburgerMenu';
 import { Gallery } from './Gallery'
 export const Hero: React.FunctionComponent = () => {
   return (
-    <div className='border-red-400 hero'>
+    <div className='hero'>
       <HamburgerMenu />
 
       <div className='flex md:h-[90vh] flex-col border2 justify-center items-centr'>
 
-        <div className='w-[84%] border2 md:w-[84.2%] mx-auto border-2 mt-[12.12rem] intro md:mt-[17rem]'>
+        <div className='w-[84%] md:w-[84.2%] mx-auto border2 mt-[12.12rem] intro md:mt-[17rem]'>
            <div className="heading">
               <h1 className='text-[#F4F4F4] leading-[120%] tracking[2px] uppercase font-black style w-[18.5625rem] md:w-[100%] heads md:text-[6rem]'>designer with a professional vision</h1>
            </div>
@@ -19,7 +19,7 @@ export const Hero: React.FunctionComponent = () => {
             <p className='text-[#aaa] w-[15.8125rem] h-[3.625rem] md:w-[29.625rem] md:h-[5.4375rem] text-[0.875rem] md:text-[1.5rem] style font-normal leading-[120%]'>Deliver seamless design and  intuitive functionality for web and mobile apps that enhance user engagement</p>
            </div>
 
-            <div className="flex border-green-500 mt-[3rem] mx-auto md:mt-[2.5rem] call">
+            <div className="flex mt-[3rem] mx-auto md:mt-[2.5rem] call">
               <Button variant='link' className='text-[#F4F4F4] text-[1rem] flex gap-[0.75rem] items-center md:text-[1.5rem] style font-bold leading-[120%] px-0'>
                   Visit my works
               <VisitArrow className='md:mt-1 md:w-[1.375rem] md:h-[1.375rem]'/>
