037517435df59f3934f392cd7e96d08cb65754e5
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-07 21:13:09 +0100

    working on the hero section

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -1,21 +1,26 @@
-import work from '@/assets/work.png'
+import work from '@/assets/work.png';
+import wor from '@/assets/wor.png'
 export const Gallery: React.FunctionComponent = () => {
     return (
         <div className="border-2 mt-[7.12rem] flex flex-col gap-[1rem] md:gap-[1.5rem]">
+
            <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[1rem] md:gap-[1.5rem]">
-            <div className='w-[90%] h-[19.5rem] md:w-[45%] md:h-[35.2rem]'>
-                <img src={work} alt="work" />
+            
+            <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
+                <img src={work} alt="work" className=" hidden md:block"/>
+                <img src={wor} alt="work" className="block md:hidden "/>
             </div>
-             <div className='w-[90%] h-[19.5rem] md:w-[45%] md:h-[35.2rem]'>
+            
+             <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
                 <img src={work} alt="work" />
             </div>
            </div>
 
            <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[1rem] md:gap-[1.5rem]">
-            <div className='w-[90%] h-[19.5rem] md:w-[45%] md:h-[35.2rem]'>
+            <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
                 <img src={work} alt="work" />
             </div>
-             <div className='w-[90%] h-[19.5rem] md:w-[45%] md:h-[35.2rem]'>
+             <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
                 <img src={work} alt="work" />
             </div>
            </div>
