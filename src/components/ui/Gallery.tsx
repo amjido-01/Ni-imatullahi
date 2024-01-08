cffd21ede0156ead2fa39c06b5bcbc67b169b1e1
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-08 04:23:31 +0100

    working on the hero section

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -1,27 +1,52 @@
 import work from '@/assets/work.png';
-import wor from '@/assets/wor.png'
+import wor from '@/assets/wor.png';
+import kustbookletlg from '@/assets/kustbookletlg.png';
+import kustbookletsm from '@/assets/kustbookletsm.png';
+import alaportfoliolg from '@/assets/alaportfoliolg.png';
+import alaportfoliosm from '@/assets/alaportfoliosm.png';
+import bookbaylg from '@/assets/bookbaylg.png';
+import bookbaysm from '@/assets/bookbaysm.png';
+import pixellg from '@/assets/pixellg.png';
+import pixelsm from '@/assets/pixelsm.png';
+import live from '@/assets/live.png'
 export const Gallery: React.FunctionComponent = () => {
     return (
-        <div className="border-2 mt-[7.12rem] flex flex-col gap-[1rem] md:gap-[1.5rem]">
+        <div className="border-2 mt-[7.12rem] flex flex-col gap-[2.54rem] md:gap-[1.5rem]">
 
-           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[1rem] md:gap-[1.5rem]">
+           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[2.54rem] md:gap-[1.5rem]">
             
             <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
-                <img src={work} alt="work" className=" hidden md:block"/>
-                <img src={wor} alt="work" className="block md:hidden "/>
+                <div>
+                    <img src={kustbookletlg} alt="work" className="hidden md:block"/>
+                    <div>
+                        <div>
+                            <h3>BookBay</h3>
+                            <p>Ui/UX Designer</p>
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+               <div>
+                 <img src={kustbookletsm} alt="work" className="block md:hidden "/>
+               </div>
             </div>
             
              <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
-                <img src={work} alt="work" />
+                <img src={alaportfoliolg} className="hidden md:block" alt="work" />
+                <img src={alaportfoliosm} className='block md:hidden' alt="work" />
             </div>
            </div>
 
-           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[1rem] md:gap-[1.5rem]">
+           <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[2.54rem] md:gap-[1.5rem]">
             <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
-                <img src={work} alt="work" />
+                <img src={bookbaylg} className='hidden md:block' alt="work" />
+                <img src={bookbaysm} className='block md:hidden' alt="work" />
             </div>
              <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
-                <img src={work} alt="work" />
+                <img src={pixellg} className='hidden md:block' alt="work" />
+                <img src={pixelsm} className='block md:hidden' alt="work" />
             </div>
            </div>
         </div>
--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero: React.FunctionComponent = () => {
 
       <div className='flex md:h-[90vh] flex-col justify-center items-centr'>
 
-        <div className='w-[90%] border-2 px-2 sm:px-0 sm:w-[85%] md:w-[84.2%] mx-auto mt-[12.12rem] intro md:mt-[17rem]'>
+        <div className='w-[90%] border2 px-2 sm:px-0 sm:w-[85%] md:w-[84.2%] mx-auto mt-[12.12rem] intro md:mt-[17rem]'>
            <div className="heading">
               <h1 className='text-[#F4F4F4] leading-[120%] tracking[2px] uppercase font-black style w-full sm:w-[80%] md:w-[100%] heads md:text-[5.6rem] lg:text-[6rem]'>designer with a professional vision</h1>
            </div>
