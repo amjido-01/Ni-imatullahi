bbbd848dda260580db7add79dc9f9a792e34ba6a
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-08 04:41:09 +0100

    working on the hero section

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -1,5 +1,4 @@
-import work from '@/assets/work.png';
-import wor from '@/assets/wor.png';
+import live from '@/assets/live.png';
 import kustbookletlg from '@/assets/kustbookletlg.png';
 import kustbookletsm from '@/assets/kustbookletsm.png';
 import alaportfoliolg from '@/assets/alaportfoliolg.png';
@@ -8,7 +7,6 @@ import bookbaylg from '@/assets/bookbaylg.png';
 import bookbaysm from '@/assets/bookbaysm.png';
 import pixellg from '@/assets/pixellg.png';
 import pixelsm from '@/assets/pixelsm.png';
-import live from '@/assets/live.png'
 export const Gallery: React.FunctionComponent = () => {
     return (
         <div className="border-2 mt-[7.12rem] flex flex-col gap-[2.54rem] md:gap-[1.5rem]">
@@ -16,21 +14,23 @@ export const Gallery: React.FunctionComponent = () => {
            <div className="flex flex-col md:flex-row justify-center items-center border-2 gap-[2.54rem] md:gap-[1.5rem]">
             
             <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
-                <div>
-                    <img src={kustbookletlg} alt="work" className="hidden md:block"/>
-                    <div>
-                        <div>
-                            <h3>BookBay</h3>
-                            <p>Ui/UX Designer</p>
-                        </div>
-                        <div>
-                            
-                        </div>
+                <div className='relative'>
+                    <img src={kustbookletlg} alt="work" className="hidden image md:block"/>
+                   <img src={kustbookletsm} alt="work" className="block image md:hidden "/>
+                   
+                   <div className='flex items-center justify-center'>
+                         <div className='absolute flex border-red-500 border-2 rder-2 md:bottom-[1.84rem] w-[70%] justify-around'>
+                    <div className='border-2 text-[#ffffff]'>
+                        <h3>KUSTBooklets</h3>
+                        <p>Frontend Developer, UI/UX designer</p>
                     </div>
+                    <div>
+                        <img src={live} className='md:w-[3.78594rem] md:h-[3.78594rem]' alt="live" />
+                    </div>
+                   </div>
+                   </div>
+                  
                 </div>
-               <div>
-                 <img src={kustbookletsm} alt="work" className="block md:hidden "/>
-               </div>
             </div>
             
              <div className='w-[94%] gala md:w-[47.5%] rounded-[1.21875rem] md:rounded-[2.34669rem] overflow-hidden border2'>
