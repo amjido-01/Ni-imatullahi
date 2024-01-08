2648946b9b3081c30d61712548998ab659928494
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-08 06:36:15 +0100

    project gallery section

--- src/components/ui/Project.tsx
+++ src/components/ui/Project.tsx
@@ -13,8 +13,8 @@ export const Project:React.FunctionComponent<projectprops> = ({deskstopImg, mobi
     <div>
         
                 <div className='relative'>
-                    <img src={deskstopImg} alt="work" className="hidden image lg:block"/>
-                   <img src={mobileImage} alt="work" className="block image lg:hidden "/>
+                    <img src={deskstopImg} alt="work" className="hidden object-cover image lg:block"/>
+                   <img src={mobileImage} alt="work" className="block object-cover image lg:hidden"/>
                    
                    <div className='flex items-center justify-center border2'>
                          <div className='absolute p-[0.65rem] md:p-[1.17rem] flex border-red-500 bg-[#00000033] backdrop-filter backdrop-blur-[29.333332061767578px] bottom-[1.5rem] md:bottom-[1.84rem] w-[80%] md:w-[70%] justify-between rounded-[0.64844rem] md:rounded-[1.17331rem] project'>
@@ -23,7 +23,7 @@ export const Project:React.FunctionComponent<projectprops> = ({deskstopImg, mobi
                         <p className='text-[0.60938rem] md:text-[1.02669rem] style font-normal mt-[0.04rem] md:mt-[0.29rem] leading-[120%] capitalize'>{role}</p>
                     </div>
                     <div>
-                        <img src={liveIcon} className='w-[1.75rem] h-[1.75rem] md:w-[3.78594rem] md:h-[3.78594rem] exclude' alt="live" />
+                        <img src={liveIcon} className='w-[1.75rem] h-[1.75rem] md:w-[3.78594rem] md:h-[3.78594rem] exclude object-cover' alt="live" />
                     </div>
                    </div>
                    </div>
