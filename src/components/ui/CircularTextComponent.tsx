4b452632c122500a328116730c223688cce01ea6
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-11 06:45:21 +0100

    adding some adjustments

--- src/components/ui/CircularTextComponent.tsx
+++ src/components/ui/CircularTextComponent.tsx
@@ -36,12 +36,13 @@ export const CircularTextComponent: React.FC = () => {
   }, [controls]);
 
   return (
-    <motion.div
-      className="relative block w-full h-[100px] border-2 border-red-500"
+    <div className=''>
+        <motion.div
+      className="w-full h-[100px] border-2 border-red-500"
       animate={controls}
     >
       <motion.div
-        className="flex flex-col border2 items-center justify-center w-[200px] h-[200px] fixed right-0 circular-text"
+        className="flex flex-col border-2 items-center justify-center w-[200px] h-[200px] fixed right-0 circular-text"
         initial={{ rotate: 0 }}
       >
         <p className="text spaced-words text-[#fff] p-4 font-extrabold leading-none text-[0.50806rem] md:text-[0.875rem] uppercase">
@@ -57,5 +58,7 @@ export const CircularTextComponent: React.FC = () => {
         </motion.div>
       </motion.div>
     </motion.div>
+    </div>
+    
   );
 };
