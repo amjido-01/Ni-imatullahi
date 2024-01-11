d86804c7b150a0139149e7d850b8c2f323566206
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-11 07:26:38 +0100

    adding some adjustments

--- src/components/ui/Me.tsx
+++ src/components/ui/Me.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import { Word } from './Word';
 // import  {CircularTextComponent}  from './CircularTextComponent';
 import CircleType from 'circletype';
-// import { motion, useAnimation } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import { Button } from './button';
 
 export const Me: React.FunctionComponent = () => {
+  const controls = useAnimation();
     useEffect(() => {
       const textElement = document.querySelector('.text') as HTMLParagraphElement | null;
       if (!textElement) {
@@ -14,7 +15,25 @@ export const Me: React.FunctionComponent = () => {
 
       const rotate = new CircleType(textElement).radius(40);
       console.log(rotate);
-    })
+
+       const handleScroll: EventListener = () => {
+      const yOffset = window.scrollY;
+      const componentOffset = textElement.getBoundingClientRect().top;
+
+      // Check if the component is in the viewport
+      if (componentOffset < window.innerHeight && componentOffset > 0) {
+        textElement.style.transform = `rotate(${yOffset * 0.15}deg)`;
+      }
+    };
+
+    // Add a scroll listener to the component
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    }, [controls])
   return (
 <div className='mt-[7.31rem] relative border-2 md:mt-[13.69rem] w-[90%] md:w-[80%] mx-auto'>
   <div>
@@ -30,14 +49,14 @@ export const Me: React.FunctionComponent = () => {
 
 
 
-  <div
+  <motion.div
       className="w-full relative h-[200px] border-2 border-red-500"
-      // animate={controls}
+      animate={controls}
     >
 
-    <div
+    <motion.div
       className="flex flexcol absolute border-2 items-center justify-center w-[200px] h-[200px] fixd right-0 circular-text"
-      // initial={{ rotate: 0 }}
+      initial={{ rotate: 0 }}
     >
         <p className="text border-2 relative top20 spaced-words text-[#fff] p-4 font-extrabold leading-none text-[0.50806rem] md:text-[0.875rem] uppercase">
           if yes Do you wanna know more?{' '}
@@ -50,8 +69,8 @@ export const Me: React.FunctionComponent = () => {
             Click me
           </Button>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
 
 </div>
   )
