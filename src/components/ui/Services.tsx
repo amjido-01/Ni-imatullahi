0895d28c92646dd23b4eee69fca5d119d4eedafd
amjido-01 <youndsadeeq10@gmail.com>
2024-01-23 14:31:53 +0100

    services section

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -4,11 +4,15 @@ import { ServicesCard } from './ServicesCard';
 export const Services:React.FunctionComponent = () => {
   const background = ' linear-gradient(90deg, #0F8A80 0%, #0B8E3C 100%)'
   return (
-    <div className='md:mt-[5.22rem] border-2'>
-       <div>
-        <ServicesCard background={background}/>
-       </div>
-
+    <div className='md:mt-[15.25rem] border-2'>
+      <div className='border-2 w-[80%] mx-auto'>
+        <div className='border-red-500 border-2 flex items-center justify-center'>
+          <ServicesCard background={background}/>
+          <div>
+            {}
+          </div>
         </div>
+      </div>
+      </div>
   )
 }
--- src/components/ui/ServicesCard.tsx
+++ src/components/ui/ServicesCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import research from '@/assets/research.svg'
 
 type Cardprops = {
     background: string;
@@ -6,9 +7,13 @@ type Cardprops = {
 
 export const ServicesCard:React.FunctionComponent<Cardprops> =({background}) => {
   return (
-    <div style={{background: background}} className='relative w-[25.96319rem] h-[20.72631rem] '>
-        <div style={{backgroundColor: 'rgba(255, 255, 255, 0.01)'}} className='absolute border-2 w-full h-full backdrop-blur-[20px] -top-10 left-10'>
-
+    <div style={{background: background}} className='relative w-[25.96319rem] rounded-[2rem] h-[20.72631rem] '>
+        <div style={{backgroundColor: 'rgba(255, 255, 255, 0.01)', boxShadow: ' 2px 2px 2px 0px rgba(255, 255, 255, 0.30) inset'}} className='absolute border-[0.5px] border-solid border-[#aaa] w-full h-full backdrop-blur-[20px] -top-10 left-10 md:py-[4.9375rem] md:px-[4rem] rounded-[1.57288rem]'>
+            <div className='border-2'>
+                <img src={research} alt="icon" />
+                <h4 className='text-[#fff] md:text-[1.25rem] md:my-[1rem] style font-bold leading-normal'>User Research and Analysis</h4>
+                <p className='text-[#fff] md:text-[0.875rem] style font-normal leading-normal'>Conducting in-depth user research, analyzing data for actionable insights, and creating user personas and journey maps to enhance the user experience</p>
+            </div>
         </div>
     </div>
   )
