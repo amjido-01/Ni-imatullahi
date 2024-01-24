62ac6f84c2589c59e9709b268923c62b279de3de
amjido-01 <youndsadeeq10@gmail.com>
2024-01-24 13:15:56 +0100

    services section

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -3,29 +3,29 @@ import { ServicesCard } from './ServicesCard';
 import research from '@/assets/research.svg'
 import prototyping from '@/assets/prototyping.svg';
 import testing from '@/assets/testing.svg';
-import visual from '@/assets/visual.svg'
+import visualDesign from '@/assets/visualDesign.svg'
 const cardData = [
   {id: 0,
     icon: research,
-    background: 'linear-gradient(90deg, #0F8A80 0%, #0B8E3C 100%)',
+    background: 'linear-gradient(76deg, #13867D 9.68%, rgba(8, 94, 40, 0.00) 90.29%)',
     title: 'User Research and Analysis',
     content: 'Conducting in-depth user research, analyzing data for actionable insights, and creating user personas and journey maps to enhance the user experience'
   },
   {id: 1,
-    background: '#93451B',
+    background: 'linear-gradient(90deg, #911308 0%, rgba(142, 19, 8, 0.00) 109.77%)',
     icon: prototyping,
     title: 'Prototyping and Wireframing',
     content: 'Creating interactive prototypes and wireframes, refining through iterative testing for user feedback, and delivering a clear roadmap to clients and development teams for the final product.'
   },
   {id: 2,
-    icon: visual,
-    background: '#606060',
+    icon: visualDesign,
+    background: ' linear-gradient(90deg, #4D4D4D -2.41%, rgba(96, 96, 96, 0.00) 108.33%)',
     title: 'Visual Design and Branding',
     content: 'Crafting visually appealing designs aligned with brand identity, selecting color schemes, typography, and imagery to enhance the user experience, and ensuring consistency across platforms for a seamless experience.'
   },
   {id: 3,
     icon: testing,
-    background: '#0557A2',
+    background: 'linear-gradient(89deg, #055094 1.23%, rgba(5, 87, 162, 0.00) 105.11%)',
     title: 'Usability Testing and Iterative Design',
     content: 'Conducting usability testing, iteratively refining designs based on user feedback, and collaborating with stakeholders and development teams to implement design improvements.'
   },
@@ -37,7 +37,8 @@ export const Services:React.FunctionComponent = () => {
     <div className='md:mt-[15.25rem] border-2'>
       <div className='border-2 w-[95%] md:w-[80%] mx-auto'>
 
-        <div className='border-red-500 border-2 flex items-center justify-center my-[5rem]'>
+        <div className='border-red-500 border-2 flex items-center justifycenter my-[5rem]'>
+          <div></div>
           <ServicesCard background={cardData[0].background} title={cardData[0].title} icon={cardData[0].icon} content={cardData[0].content}/>
         </div>
 
