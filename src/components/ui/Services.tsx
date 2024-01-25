a674dc56553887f8dada014b5019947f0e5eb51e
amjido-01 <youndsadeeq10@gmail.com>
2024-01-25 13:19:41 +0100

    logest prefix

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -6,6 +6,8 @@ import testing from '@/assets/testing.svg';
 import visualDesign from '@/assets/visualDesign.svg';
 import proud from '@/assets/proud.svg'
 import proudsmlf from '@/assets/proudsmlf.svg'
+import proudrg from '@/assets/proudrg.svg'
+import proudrgsm from '@/assets/proudrgsm.svg';
 const cardData = [
   {id: 0,
     icon: research,
@@ -36,44 +38,56 @@ const cardData = [
 
 export const Services:React.FunctionComponent = () => {
   return (
-    <div className='md:mt-[15.25rem] border-2 flex justify-center'>
+    <div className='md:mt-[15.25rem] border2 flex justify-center'>
 
-      <div className='border-2 flex justify-end  border-red-500 md:w-[12.5%]'
+      {/* <div className='border2 flex justify-end  border-red-500 md:w-[12.5%]'
       >
-
-      <div className='border-2 h-fit'>
+      <div className='border2 h-fit'>
       <img className='block sm:hidden' src={proudsmlf} alt="text" />
       <img className='hidden sm:block' src={proud} alt="text" />
           </div>
-      </div>
+      </div> */}
 
-      <div className='border-2 border-pink-400 w-[95%] md:w-[75%] mxauto'>
-        <div className='border-red-500 border2 flex items-center justify-center my-[44px] md:my-[55px] h-[400px]'>
-          
-          <div className='border-2 flex md:justify-center'>
-          <ServicesCard background={cardData[0].background} title={cardData[0].title} icon={cardData[0].icon} content={cardData[0].content}/>
-          </div>
+      <div className='border2 border-pink-400 w-[100%] md:w-[75%] mxauto'>
+
+       <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-[55px] h-[400px]'>
+        <div className='border2 h-full'>
+        <img className='block sm:hidden' src={proudsmlf} alt="text" />
+      <img className='hidden sm:block' src=     {proud} alt="text" />
         </div>
 
-        <div className='border-red-500 border-2 flex flex-col gap-[5rem] sm:items-center lg:flex-row justify-center my-[44px] md:my-[65px] md:gap-[6rem]'>
+         <div className='border2 flex md:justify-center w-[95%] pr5 sm:pr-0'>
+         <ServicesCard background={cardData[0].background} title={cardData[0].title} icon={cardData[0].icon} content={cardData[0].content}/>
+         </div>
+        </div>
+
+        <div className='border-red-500 border2 flex flex-col pl-5 gap-[5rem] sm:items-center lg:flex-row justify-center my-[44px] md:my-[65px] md:gap-[6rem]'>
           <ServicesCard background={cardData[1].background} title={cardData[1].title} icon={cardData[1].icon} content={cardData[1].content}/>
           <ServicesCard background={cardData[2].background} title={cardData[2].title} icon={cardData[2].icon} content={cardData[2].content}/>
         </div>
 
-        <div className='border-red-500 flex items-center justify-center my-[44px] md:my-[55px] h-[400px]'>
-          <ServicesCard background={cardData[3].background} title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
+        <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-[55px] gap-1 h-[400px]'>
+
+         <div className='border2 flex  justify-center w-[95%] '>
+         <ServicesCard background={cardData[3].background} title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
+         </div>
+
+         <div className='border2 h-full'>
+         <img className='block sm:hidden' src={proudrgsm} alt="text" />
+      <img className='hidden sm:block' src={proudrg} alt="text" />
+          </div>
         </div>
 
       </div>
 
-      <div className='border-2 flex flex-col justify-end  border-yellow-500 md:w-[12.5%] '>
+      {/* <div className='border2 flex flex-col justify-end  border-yellow-500 md:w-[12.5%] '>
 
-      <div className='border-2 justify-end w-fit'>
-      <img className='block sm:hidden' src={proudsmlf} alt="text" />
-      <img className='hidden sm:block' src={proud} alt="text" />
+      <div className='border2 justify-end w-fit'>
+      <img className='block sm:hidden' src={proudrgsm} alt="text" />
+      <img className='hidden sm:block' src={proudrg} alt="text" />
           </div>
           
-      </div>
+      </div> */}
 
       </div>
   )
