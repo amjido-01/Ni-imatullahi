f57b8d794abc90255e325c5f7a84c1589010653c
amjido-01 <youndsadeeq10@gmail.com>
2024-01-26 10:43:39 +0100

    logest prefix

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -14,24 +14,28 @@ import testinglg from '@/assets/testinglg.svg';
 import prototypinglg from '@/assets/prototypinglg.svg'
 const cardData = [
   {id: 0,
+    borderColor: '#19B3A7',
     largeIcon: researchlg,
     icon: research,
     title: 'User Research and Analysis',
     content: 'Conducting in-depth user research, analyzing data for actionable insights, and creating user personas and journey maps to enhance the user experience'
   },
   {id: 1,
+    borderColor: '#C11A0B',
     largeIcon: prototypinglg,
     icon: prototyping,
     title: 'Prototyping and Wireframing',
     content: 'Creating interactive prototypes and wireframes, refining through iterative testing for user feedback, and delivering a clear roadmap to clients and development teams for the final product.'
   },
   {id: 2,
+    borderColor: '#CC9F00',
     largeIcon: visualDesignlg,
     icon: visualDesign,
     title: 'Visual Design and Branding',
     content: 'Crafting visually appealing designs aligned with brand identity, selecting color schemes, typography, and imagery to enhance the user experience, and ensuring consistency across platforms for a seamless experience.'
   },
   {id: 3,
+    borderColor: '#056BC7',
     largeIcon: testinglg,
     icon: testing,
     title: 'Usability Testing and Iterative Design',
@@ -42,7 +46,7 @@ const cardData = [
 
 export const Services:React.FunctionComponent = () => {
   return (
-    <div className='mt-[10rem] md:pt-[12.25rem] border2 flex justify-center service'>
+    <div className='mt-[8rem] border2 flex justify-center service'>
 
       {/* <div className='border2 flex justify-end  border-red-500 md:w-[12.5%]'
       >
@@ -54,26 +58,26 @@ export const Services:React.FunctionComponent = () => {
 
       <div className='border2 border-pink-400 w-[100%] md:w-[75%] mxauto'>
 
-       <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-[55px] h-[400px]'>
-        <div className='border2 h-full'>
-        <img className='block mt-5 sm:hidden' src={proudsmlf} alt="text" />
-      <img className='hidden sm:block' src=     {proud} alt="text" />
+       <div className='border2 border-red-500 flex items-center justify-center mt-[44px] mb-4 md:my-[55px] h-[400px]'>
+        <div className='h-full'>
+        <img className='block mt-16 sm:hidden' src={proudsmlf} alt="text" />
+        <img className='hidden sm:block mt-10' src={proud} alt="text" />
         </div>
 
          <div className='border2 flex sm:justify-center w-[95%] pr5 sm:pr-0'>
-         <ServicesCard title={cardData[0].title} largeIcon={cardData[0].largeIcon} icon={cardData[0].icon} content={cardData[0].content}/>
+         <ServicesCard borderColor={cardData[0].borderColor} title={cardData[0].title} largeIcon={cardData[0].largeIcon} icon={cardData[0].icon} content={cardData[0].content}/>
          </div>
         </div>
 
-        <div className='border-red-500 border2 flex flex-col pl-5 gap-[5rem] sm:items-center lg:flex-row justify-center my-[44px] md:my-[65px] md:gap-[6rem]'>
-          <ServicesCard title={cardData[1].title} largeIcon={cardData[1].largeIcon} icon={cardData[1].icon} content={cardData[1].content}/>
-          <ServicesCard largeIcon={cardData[2].largeIcon} title={cardData[2].title} icon={cardData[2].icon} content={cardData[2].content}/>
+        <div className='border-red-500 border2 flex flex-col pl-5 gap-[5rem] sm:items-center lg:flex-row justify-center mb-[44px] md:my-[8rem] md:gap-[10rem]'>
+          <ServicesCard borderColor={cardData[1].borderColor} title={cardData[1].title} largeIcon={cardData[1].largeIcon} icon={cardData[1].icon} content={cardData[1].content}/>
+          <ServicesCard largeIcon={cardData[2].largeIcon}  borderColor={cardData[2].borderColor} title={cardData[2].title} icon={cardData[2].icon} content={cardData[2].content}/>
         </div>
 
-        <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-[55px] gap-1 h-[400px]'>
+        <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-4 gap-1 h-[400px]'>
 
          <div className='border2 flex  justify-center w-[95%] '>
-         <ServicesCard largeIcon={cardData[3].largeIcon} title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
+         <ServicesCard largeIcon={cardData[3].largeIcon}  borderColor={cardData[3].borderColor} title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
          </div>
 
          <div className='border2 h-full relative'>
--- src/components/ui/ServicesCard.tsx
+++ src/components/ui/ServicesCard.tsx
@@ -5,16 +5,17 @@ type Cardprops = {
     icon: string;
     title: string;
     content: string;
+    borderColor: string
 }
 
-export const ServicesCard:React.FunctionComponent<Cardprops> =({largeIcon, icon, title, content}) => {
+export const ServicesCard:React.FunctionComponent<Cardprops> =({largeIcon, icon, title, content, borderColor}) => {
   return (
-    <div className='card rounded-[2rem] w-[90%] mx-aut sm:w-[30rem] md:w-[65%] lg:w-[40%] h-[320px] backdrop-brightness-50 border-2 border-red-500'>
+    <div style={{boxShadow: '5px 5px 30px 0px rgba(0, 0, 0, 0.25)', border: `2px solid ${borderColor}`}} className={`card rounded-[2rem] w-[90%] mx-aut sm:w-[30rem] md:w-[65%] lg:w-[40%]`}>
 
-          <div className='border-2 h-full p-[50px]'>
+          <div className='h-full p-[40px] md:p-[50px] colored'>
                 <img src={icon} className='md:hidden' alt="icon" />
                 <img src={largeIcon} className="hidden md:block" alt="icon" />
-                <h4 className='text-[#fff] tex-[1rem] md:text-[1.25rem] md:my-[1rem] style font-bold leading-normal'>{title}</h4>
+                <h4 className='text-[#fff] tex-[1rem] md:text-[1.25rem] my-[1rem] style font-bold leading-normal'>{title}</h4>
                 <p className='text-[#fff] text-[0.80rem] md:text-[0.875rem] style font-normal leading-normal'>{content}</p>
             </div>
 
