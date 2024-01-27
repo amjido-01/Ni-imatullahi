7a2ede35092239909fdc8c1f17651bb664a5bc72
amjido-01 <youndsadeeq10@gmail.com>
2024-01-27 09:53:50 +0100

    commit

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -8,35 +8,27 @@ import proud from '@/assets/proud.svg'
 import proudsmlf from '@/assets/proudsmlf.svg'
 import proudrg from '@/assets/proudrg.svg'
 import proudrgsm from '@/assets/proudrgsm.svg';
-import researchlg from '@/assets/researchlg.svg'
-import visualDesignlg from '@/assets/visualDesignlg.svg'
-import testinglg from '@/assets/testinglg.svg';
-import prototypinglg from '@/assets/prototypinglg.svg'
+// import researchlg from '@/assets/researchlg.svg'
+// import visualDesignlg from '@/assets/visualDesignlg.svg'
+// import testinglg from '@/assets/testinglg.svg';
+// import prototypinglg from '@/assets/prototypinglg.svg'
 const cardData = [
   {id: 0,
-    borderColor: '#19B3A7',
-    largeIcon: researchlg,
     icon: research,
     title: 'User Research and Analysis',
     content: 'Conducting in-depth user research, analyzing data for actionable insights, and creating user personas and journey maps to enhance the user experience'
   },
   {id: 1,
-    borderColor: '#C11A0B',
-    largeIcon: prototypinglg,
     icon: prototyping,
     title: 'Prototyping and Wireframing',
     content: 'Creating interactive prototypes and wireframes, refining through iterative testing for user feedback, and delivering a clear roadmap to clients and development teams for the final product.'
   },
   {id: 2,
-    borderColor: '#CC9F00',
-    largeIcon: visualDesignlg,
     icon: visualDesign,
     title: 'Visual Design and Branding',
     content: 'Crafting visually appealing designs aligned with brand identity, selecting color schemes, typography, and imagery to enhance the user experience, and ensuring consistency across platforms for a seamless experience.'
   },
   {id: 3,
-    borderColor: '#056BC7',
-    largeIcon: testinglg,
     icon: testing,
     title: 'Usability Testing and Iterative Design',
     content: 'Conducting usability testing, iteratively refining designs based on user feedback, and collaborating with stakeholders and development teams to implement design improvements.'
@@ -48,14 +40,6 @@ export const Services:React.FunctionComponent = () => {
   return (
     <div className='mt-[8rem] border2 flex justify-center service'>
 
-      {/* <div className='border2 flex justify-end  border-red-500 md:w-[12.5%]'
-      >
-      <div className='border2 h-fit'>
-      <img className='block sm:hidden' src={proudsmlf} alt="text" />
-      <img className='hidden sm:block' src={proud} alt="text" />
-          </div>
-      </div> */}
-
       <div className='border2 border-pink-400 w-[100%] md:w-[75%] mxauto'>
 
        <div className='border2 border-red-500 flex items-center justify-center mt-[44px] mb-4 md:my-[55px] h-[400px]'>
@@ -65,19 +49,19 @@ export const Services:React.FunctionComponent = () => {
         </div>
 
          <div className='border2 flex sm:justify-center w-[95%] pr5 sm:pr-0'>
-         <ServicesCard borderColor={cardData[0].borderColor} title={cardData[0].title} largeIcon={cardData[0].largeIcon} icon={cardData[0].icon} content={cardData[0].content}/>
+         <ServicesCard  title={cardData[0].title}  icon={cardData[0].icon} content={cardData[0].content}/>
          </div>
         </div>
 
         <div className='border-red-500 border2 flex flex-col pl-5 gap-[5rem] sm:items-center lg:flex-row justify-center mb-[44px] md:my-[8rem] md:gap-[10rem]'>
-          <ServicesCard borderColor={cardData[1].borderColor} title={cardData[1].title} largeIcon={cardData[1].largeIcon} icon={cardData[1].icon} content={cardData[1].content}/>
-          <ServicesCard largeIcon={cardData[2].largeIcon}  borderColor={cardData[2].borderColor} title={cardData[2].title} icon={cardData[2].icon} content={cardData[2].content}/>
+          <ServicesCard title={cardData[1].title}  icon={cardData[1].icon} content={cardData[1].content}/>
+          <ServicesCard  title={cardData[2].title} icon={cardData[2].icon} content={cardData[2].content}/>
         </div>
 
         <div className='border2 border-red-500 flex items-center justify-center my-[44px] md:my-4 gap-1 h-[400px]'>
 
          <div className='border2 flex  justify-center w-[95%] '>
-         <ServicesCard largeIcon={cardData[3].largeIcon}  borderColor={cardData[3].borderColor} title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
+         <ServicesCard  title={cardData[3].title} icon={cardData[3].icon} content={cardData[3].content}/>
          </div>
 
          <div className='border2 h-full relative'>
@@ -87,15 +71,6 @@ export const Services:React.FunctionComponent = () => {
         </div>
 
       </div>
-
-      {/* <div className='border2 flex flex-col justify-end  border-yellow-500 md:w-[12.5%] '>
-
-      <div className='border2 justify-end w-fit'>
-      <img className='block sm:hidden' src={proudrgsm} alt="text" />
-      <img className='hidden sm:block' src={proudrg} alt="text" />
-          </div>
-          
-      </div> */}
 
       </div>
   )
--- src/components/ui/ServicesCard.tsx
+++ src/components/ui/ServicesCard.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 
 type Cardprops = {
-    largeIcon: string;
     icon: string;
     title: string;
     content: string;
-    borderColor: string
+    background?: string;
 }
 
-export const ServicesCard:React.FunctionComponent<Cardprops> =({largeIcon, icon, title, content, borderColor}) => {
+export const ServicesCard:React.FunctionComponent<Cardprops> =({ icon, title, content}) => {
   return (
-    <div style={{boxShadow: '5px 5px 30px 0px rgba(0, 0, 0, 0.25)', border: `2px solid ${borderColor}`}} className={`card rounded-[2rem] w-[90%] mx-aut sm:w-[30rem] md:w-[65%] lg:w-[40%]`}>
+    <div style={{boxShadow: '5px 5px 30px 0px rgba(0, 0, 0, 0.25)'}} className={`card rounded-[2rem] w-[90%] mx-aut sm:w-[30rem] md:w-[65%] lg:w-[40%] border-[2px] border-solid`}>
 
           <div className='h-full p-[40px] md:p-[50px] colored'>
                 <img src={icon} className='md:hidden' alt="icon" />
-                <img src={largeIcon} className="hidden md:block" alt="icon" />
                 <h4 className='text-[#fff] tex-[1rem] md:text-[1.25rem] my-[1rem] style font-bold leading-normal'>{title}</h4>
                 <p className='text-[#fff] text-[0.80rem] md:text-[0.875rem] style font-normal leading-normal'>{content}</p>
             </div>
-
-        {/* <div style={{backgroundColor: 'rgba(255, 255, 255, 0.01)', boxShadow: ' 2px 2px 2px 0px rgba(255, 255, 255, 0.30) inset'}} className='flex h-full w-full relative left-5 md:left-8 bottom-5 md:bottom-8 items-center justify-center border-[0.1px] border-solid border-[#7a7a7a]  backdrop-blur-[20px] rounded-[1.57288rem] px-[10x] p-10 md:p-12'>
-          
-        </div> */}
     </div>
   )
 }
