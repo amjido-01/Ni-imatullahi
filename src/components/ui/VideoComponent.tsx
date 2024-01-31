048dad036a9b37495937ad64dfbab13272176425
amjido-01 <youndsadeeq10@gmail.com>
2024-01-31 18:30:38 +0100

    commit

--- src/components/ui/VideoComponent.tsx
+++ src/components/ui/VideoComponent.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export const VideoComponent = () => {
   return (
     <div className='border-2 flex items-center justify-center mt-[132px] md:mt-[293px]'>
-         <video width="90%" height='648px' controls>
-        <source src=' "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"' type="video/mp4" />
+         <video width="90%"  height='648px' controls>
+        <source src='https://www.youtube.com/watch?v=DsP9bH7HHh4' type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
