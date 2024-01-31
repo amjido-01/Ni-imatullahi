5c17104519a27bf4598c7172c8625340b495f92f
amjido-01 <youndsadeeq10@gmail.com>
2024-01-31 18:55:48 +0100

    commit

--- src/components/ui/VideoComponent.tsx
+++ src/components/ui/VideoComponent.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const VideoComponent = () => {
   return (
     <div className='border2 flex items-center justify-center mt-[132px] md:mt-[293px]'>
-         <video width="90%"  height='648px' controls>
+         <video width="90%"  height='90%' controls>
         <source src='https://www.youtube.com/watch?v=DsP9bH7HHh4' type="video/mp4" />
         Your browser does not support the video tag.
       </video>
