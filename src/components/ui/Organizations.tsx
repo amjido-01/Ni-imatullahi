e6294d7d912a24e9f363eb030c2d1523bf5728a7
amjido-01 <youndsadeeq10@gmail.com>
2024-02-02 14:11:28 +0100

    commit

--- src/components/ui/Organizations.tsx
+++ src/components/ui/Organizations.tsx
@@ -13,12 +13,11 @@ import airbnb from '@/assets/airbnb.png';
 
 export const Organizations:React.FunctionComponent = () => {
   return (
-    <div className='border-2 mt-[146px] md:mt-[209px]'>
-       <div className='border-2 w-full md:w-[80%] mx-auto'>
+    <div className='border2 mt-[146px] md:mt-[209px]'>
+       <div className='border2 w-full md:w-[80%] mx-auto'>
        <h3 className='text-[#AAAAAA] w-[90%] md:w-full  mx-auto text-[0.625rem] md:text-[1rem]'>Work experiences</h3>
         <hr className="white-hr w-[90%] md:w-full mx-auto mb-[44px] md:mb-[80px]" />
-        <div className='border-2 w-full md:w-[80%] mx-auto flex flex-col gap-[24px] md:gap-[54px]'>
-            <div className='flex items-center overflow-x-auto justify-center gap-[24px] md:gap-[96px]'>
+        <div className='flex items-center overflow-x-auto justify-center gap-[24px] md:gap-[96px]'>
                 <div>
                 <img src={gdscsm} className='block md:hidden' alt='org'/>
                 <img src={gdsc} className='hidden md:block' alt="org"/>
@@ -32,17 +31,18 @@ export const Organizations:React.FunctionComponent = () => {
                 <img src={startbench} className='hidden md:block' alt="org"/>
                 </div>
             </div>
-            <div className='flex gap-[31.03px] md:gap-[52px] justify-center items-center justifycenter overflow-x-auto'>
+
+         <div className='mt-[24px] md:mt-[52px] flex justify-between md:gap-[52px] justifycenter items-center justifycenter overflow-x-auto gap-4'>
                 <div>
                 {/* <img src={gdscsm} className='block border-2 md:hidden' alt='org'/> */}
                 <img src={airbnb} className='' alt="org"/>
                 </div>
                 <div>
                 {/* <img src={gdscsm} className='block border-2 md:hidden' alt='org'/> */}
-                <img src={amazon} className='w[65.63px] h[28.64px] object-scaledown object-cover' alt="org"/>
+                <img src={amazon} className='w[65.63px] h[28.64px]' alt="org"/>
                 </div>
                 <div>
-                {/* <img src={gdscsm} className='block border-2 md:hidden' alt='org'/> */}
+                {/* <img src={teslasm} className='block md:hidden' alt='org'/> */}
                 <img src={tesla} className='' alt="org"/>
                 </div>
                 <div>
@@ -54,8 +54,6 @@ export const Organizations:React.FunctionComponent = () => {
                 <img src={stripe} className='' alt="org"/>
                 </div>
             </div>
-            
-        </div>
        </div>
     </div>
   )
