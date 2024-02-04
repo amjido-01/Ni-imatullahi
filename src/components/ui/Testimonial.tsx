3bf4cea6b74c4acdcd9c9c66a1376a06082c3f55
amjido-01 <youndsadeeq10@gmail.com>
2024-02-04 15:50:52 +0100

    commit

--- src/components/ui/Testimonial.tsx
+++ src/components/ui/Testimonial.tsx
@@ -7,13 +7,13 @@ import {
 
 export const Testimonial = () => {
   return (
-    <Card>
-    <CardContent className='flex gap-[19px] md:gap-[14px]'>
+    <Card className=' rounded-t-[40px]'>
+    <CardContent className='flex gap-[19px] md:gap-[14px] border-2 h-full'>
       <div className='mt-[20px] md:mt-[45px]'>
         <img className='w-[70px] h-[70px] md:w-[161px] md:h-[161px]' src={ala} alt="" />
       </div>
-      <div className='border-2 border-red-500 flex items-center h-full mt-[43px] md:mt-[101px]'>
-        <div>
+      <div className='flex itemscenter h-full '>
+        <div className='border-2 mt-[43px] md:mt-[100px]'>
         <div>
             <h3 className='text-[10.53px] font-bold leading-[12.63px] md:text-[24px] md:leading-[28.8px] capitalize'>Abubakar Muhammad</h3>
             <p className='text-[#AAAAAA] mt-[2px] md:mt-[4px] text-[8px] font-normal leading-[9.6px] md:text-[14px] md:leading-[16.8px]'>Software Engineer</p>
