48416278d28122eb5cd3b20d605932e1aa188517
amjido-01 <youndsadeeq10@gmail.com>
2024-02-09 20:42:01 +0100

    commit

--- src/components/ui/VideoPlayer.tsx
+++ src/components/ui/VideoPlayer.tsx
@@ -5,7 +5,7 @@ import thumbnail from '@/assets/thumbnail.png'
 export const VideoPlayer = () => {
   return (
     <div className='mt-[132px] md:mt-[354px]'>
-      <video id='vid' poster={thumbnail} muted loop controls className='w-[90%]  h-1/2 mx-auto rounded-[16px] md:rounded-[32px] '>
+      <video id='vid' poster={thumbnail} controlsList='nofullscreen nodownload noremoteplayback noplaybackrate' disablePictureInPicture disableRemotePlayback loop controls className='w-[90%] h-1/2 mx-auto rounded-[16px] md:rounded-[32px] '>
         <source src={vid} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
