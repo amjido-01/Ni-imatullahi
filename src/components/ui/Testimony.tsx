2d74689e1ba31e564ba6cdceab153f59eff5a75f
amjido-01 <youndsadeeq10@gmail.com>
2024-02-17 14:21:52 +0100

    commit

--- src/components/ui/Testimony.tsx
+++ src/components/ui/Testimony.tsx
@@ -24,10 +24,16 @@ const cardData = [
 
 export const Testimony: React.FunctionComponent = () => {
   return (
-    <div className="relative flex flex-col justify-center border rounded-[17px] md:rounded-[36px] my-0 mx-auto parent w-[90%] md:w-[95%] h-[369px] md:h-[862px] mt-[159px] md:mt-[263px]">
-      <h2 className="text-[22.61px] leading-[27.13px] text-[#F4F4F4] md:text-[40px] md:leading-[48px] text-center">Testimonies</h2>
+    <div className="relative flex flex-col justify-center border rounded-[17px] md:rounded-[36px] my-0 mx-auto parent w-[90%] md:w-[95%] h-[670px] md:h-[1200px] mt-[159px] md:mt-[263px]">
+      <h2 className="text-[22.61px] mt-[19px] md:mt-[69px] leading-[27.13px] text-[#F4F4F4] md:text-[40px] md:leading-[48px] text-center">Testimonies</h2>
       {/* Heading for the testimonials section. */}
-      <div className="h-[80%] md:h-[70%] child w-[100vw] relative flex flex-col justify-center">
+      <div className="h-[80%] border-2 mt-[31px] md:mt-[72px] md:h-[70%] relative flex flex-col justify-center">
+      <Marquee pauseOnHover={true} speed={30} direction='right'>
+       {cardData.map((card) => <Testimonial key={card.id} img={card.img} name={card.name} role={card.role} quote={card.quote} />)}
+      </Marquee>
+      </div>
+
+      <div className="h-[80%] md:h-[70%] md:mb-[72px] md:mt-[72px] relative flex flex-col justify-center">
       <Marquee pauseOnHover={true} speed={30}>
        {cardData.map((card) => <Testimonial key={card.id} img={card.img} name={card.name} role={card.role} quote={card.quote} />)}
       </Marquee>
